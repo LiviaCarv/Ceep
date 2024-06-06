66a13f7891718e28525805f067391fe8c35796fc
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-06-05 20:13:33 -0400

    Implement note saving to Web API
    - Adjust the code to use the repository instead of the DAO for note insertion
    - Replace DAO calls with repository calls
    - Implement the Web Client behavior to save the note information to the Web API using Retrofit

--- app/src/main/java/br/com/alura/ceep/repository/NotaRepository.kt
+++ app/src/main/java/br/com/alura/ceep/repository/NotaRepository.kt
@@ -17,4 +17,17 @@ class NotaRepository(private val dao: NotaDao,
             dao.salva(notas)
         }
     }
+
+    fun buscaPorId(id: String): Flow<Nota> {
+        return  dao.buscaPorId(id)
+    }
+
+    suspend fun remove(id: String) {
+        dao.remove(id)
+    }
+
+    suspend fun salva(nota: Nota) {
+        dao.salva(nota)
+        webClient.salva(nota)
+    }
 }
--- app/src/main/java/br/com/alura/ceep/webclient/NotaWebClient.kt
+++ app/src/main/java/br/com/alura/ceep/webclient/NotaWebClient.kt
@@ -2,6 +2,7 @@ package br.com.alura.ceep.webclient
 
 import android.util.Log
 import br.com.alura.ceep.model.Nota
+import br.com.alura.ceep.webclient.model.NotaRequisicao
 import br.com.alura.ceep.webclient.services.NotaService
 
 private const val TAG = "NotaWebClient"
@@ -20,5 +21,19 @@ class NotaWebClient {
             null
         }
 
+    }
+
+    suspend fun salva(nota: Nota) {
+        try {
+            val resposta = notaService.salva(nota.id, NotaRequisicao(
+                nota.titulo, nota.descricao, nota.imagem
+            ))
+            if (!resposta.isSuccessful) {
+                Log.i(TAG, "Falha ao tentar salvar", )
+            }
+
+        } catch (e: Exception) {
+            Log.i(TAG, "Erro: ${e.message}")
+        }
     }
 }
